d10fc7c2aab94f097d5a2eaf477ba8476270e3dd
wandji20 <wbertrand10@gmail.com>
2021-07-17 15:40:03 +0100

    Add logout user action

--- src/redux/actions/logInAction.js
+++ src/redux/actions/logInAction.js
@@ -1,40 +1,43 @@
-import { 
+import {
   LOGIN_USER,
   LOGIN_USER_SUCCESS,
-  LOGIN_USER_FAILURE, } from "../constants";
+  LOGIN_USER_FAILURE,
+} from '../constants';
 
-const logInUser = () => {
-  return {
-    type: LOGIN_USER,
+const logInUser = () => ({
+  type: LOGIN_USER,
+});
+
+const logInUserSuccess = (response) => ({
+  type: LOGIN_USER_SUCCESS,
+  payload: response,
+});
+
+const logInUserFailure = (error) => ({
+  type: LOGIN_USER_FAILURE,
+  payload: error,
+});
+
+const url = 'http://localhost:3001/api/v1/login';
+
+const getLogInDetails = (details) => async (dispatch) => {
+  dispatch(logInUser());
+  try {
+    const response = await fetch(
+      url,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(details),
+      },
+    );
+    const data = await response.json();
+    dispatch(logInUserSuccess(data));
+  } catch (error) {
+    dispatch(logInUserFailure(error));
   }
-}
-
-const logInUserSuccess = (response) => {
-  return {
-    type: LOGIN_USER_SUCCESS,
-    payload: response, 
-  }
-}
-
-const logInUserFailure = (error) => {
-  return {
-    type: LOGIN_USER_FAILURE,
-    payload: error, 
-  }
-}
-
-
-const getLogInDetails = () => {
-  return async () => {
-    dispatch(logInUser());
-    try{
-      const response = await fetch(url);
-      const data = await response.json();
-      dispatch(logInUserSuccess(data))
-    } catch(error) {
-      dispatch(logInUserFailure(error))
-    }
-  }
-}
+};
 
 export default getLogInDetails;
