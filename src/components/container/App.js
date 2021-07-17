016cd90e19ffd830f0e76a846c8661bd15ae03e0
wandji20 <wbertrand10@gmail.com>
2021-07-17 14:33:01 +0100

    Add Home component

--- src/components/container/App.js
+++ src/components/container/App.js
@@ -1,15 +1,23 @@
-import { Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import Nav from '../presentation/Nav';
+import Home from './Home';
 import SignUpForm from '../presentation/SignUpForm';
 import Footer from './Footer';
 
 function App() {
   return (
-    <>
+    <Router>
       <Nav />
-      <SignUpForm />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/sign_up" component={SignUpForm} />
+      </Switch>
       <Footer />
-    </>
+    </Router>
   );
 }
 
