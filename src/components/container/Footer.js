ccc30d018aef97308d52782af6c8b77828bdb16e
wandji20 <wbertrand10@gmail.com>
2021-07-18 19:54:40 +0100

    Add dummy data to app footer and update css for proper display

--- src/components/container/Footer.js
+++ src/components/container/Footer.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="container footer bg-info">
-    <div className="row">
-      footer
+  <footer className=" d-flex flex-column justify-content-center container remove-padding footer bg-info">
+    <div className="row align-items-center">
+      <div className="col-3 d-flex justify-content-center flex-column align-items-center">
+        <span>
+          +
+        </span>
+        <span>
+          Car
+        </span>
+      </div>
+      <div className="col-6 d-flex justify-content-between align-items-center">
+        <span className="w-50">
+          progress bar
+        </span>
+        <span className="w-50">
+          other details
+        </span>
+      </div>
+      <div className="col-3">
+        something
+      </div>
     </div>
   </footer>
 );
