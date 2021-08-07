8ae31ae9cbf0066aa0fa15831cbda598ecb6003f
wandji20 <wbertrand10@gmail.com>
2021-08-08 00:25:12 +0100

    Update test for user action

--- src/test/actions/userAction.test.js
+++ src/test/actions/userAction.test.js
@@ -1,40 +1,39 @@
-// import configureMockStore from 'redux-mock-store';
-// import thunk from 'redux-thunk';
-// import { signUpUser, logInUser, logInUserSession } from '../../redux/actions/userAction';
+import axios from 'axios';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { signUpUser, logInUser } from '../../redux/actions/userAction';
 
-// const middlewares = [thunk];
-// const mockStore = configureMockStore(middlewares);
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
+jest.mock('axios');
 describe('when a user signs up', () => {
-  global.fetch = jest.fn(() => Promise.resolve({
-    json: () => Promise.resolve([]),
-  }));
+
   it('fires a sign up request action', async () => {
-    // const store = mockStore({});
-    // return store.dispatch(signUpUser({ name: 'Wandji', userID: '@wandji' }))
-    //   .then(() => {
-    //     const actions = store.getActions();
-    //     expect(actions[0]).toEqual({ type: 'SIGN_UP_USER_REQUEST' });
-    //   });
+    const resp = { auth_token: 'nnnnnnnn' };
+    axios.post = jest.fn()
+    axios.post.mockResolvedValue(resp);
+    const store = mockStore({});
+    return store.dispatch(signUpUser({ name: 'Wandji', userID: '@wandji' }))
+      .then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({ type: 'SIGN_UP_USER' });
+      });
   });
 });
 
-// describe('when a user logs in', () => {
-//   it('fires a log in request action', async () => {
-//     const store = mockStore({});
-//     return store.dispatch(getLogInDetails({ userID: '@wandji' }))
-//       .then(() => {
-//         const actions = store.getActions();
-//         expect(actions[0]).toEqual({ type: 'LOGIN_USER' });
-//       });
-//   });
-// });
+describe('when a user logs in', () => {
+  it('fires a log in request action', async () => {
+    const resp = { auth_token: 'nnnnnnnn' };
+    axios.post = jest.fn()
+    axios.post.mockResolvedValue(resp);
+    const store = mockStore({});
+    return store.dispatch(logInUser({ userID: '@wandji', password: 'kkkkkkkkkkk' }))
+      .then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({ type: 'LOGIN_USER' });
+      });
+  });
+});
 
-// describe('when user logs out', () => {
-//   const store = mockStore({});
-//   it('fires log out action', () => {
-//     store.dispatch(logOutUser());
-//     const actions = store.getActions();
-//     expect(actions[0]).toEqual({ type: 'LOGOUT_USER' });
-//   });
-// });
+
