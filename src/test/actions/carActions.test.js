2fda9b712afee552b14283515dcfbdab224fb0b6
wandji20 <wbertrand10@gmail.com>
2021-08-08 00:08:10 +0100

    Update test suits for cars actions

--- src/test/actions/carActions.test.js
+++ src/test/actions/carActions.test.js
@@ -1,57 +1,71 @@
-// import configureMockStore from 'redux-mock-store';
-// import thunk from 'redux-thunk';
-// import {
-//   deleteCar, getCars, postCar, updatePart,
-// } from '../../redux/actions/carsAction';
+import axios from 'axios';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {
+  deleteCar, getCars, postCar, updatePart,
+} from '../../redux/actions/carsAction';
 
-// const middlewares = [thunk];
-// const mockStore = configureMockStore(middlewares);
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
-describe('when user create car', () => {
-  it('fires a post car action', async () => {
-    // const store = mockStore({});
-    // return store.dispatch(postCar({ name: 'Matrix' }))
-    //   .then(() => {
-    //     const actions = store.getActions();
-    //     expect(actions[0]).toEqual({ type: 'POST_CAR' });
-    //   });
+
+describe('when user loads all cars', () => {
+  
+  jest.mock('axios');
+  
+  it('fires get cars action request', async () => {
+
+    const resp = { cars: [], parts: [] };
+    axios.get = jest.fn()
+    axios.get.mockResolvedValue(resp);
+
+    const store = mockStore([]);
+    return store.dispatch(getCars())
+    .then(() => {
+      const actions = store.getActions();
+        expect(actions[0]).toEqual({ type: 'GET_CARS' });
+      })
   });
 });
 
-// describe('when user deletes car', () => {
-//   it('fires a delete car action', async () => {
-//     const store = mockStore({});
-//     return store.dispatch(deleteCar({ name: 'Matrix' }))
-//       .then(() => {
-//         const actions = store.getActions();
-//         expect(actions[0]).toEqual({ type: 'DELETE_CAR' });
-//       });
-//   });
-// });
+describe('when user create car', () => {
+  it('fires a post car action', async () => {
+    const resp = { car: {} };
+    axios.post = jest.fn()
+    axios.post.mockResolvedValue(resp);
+    const store = mockStore({});
+    return store.dispatch(postCar({ name: 'Matrix' }))
+      .then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({ payload: 0, type: 'SET_FILTER_INDEX' });
+        expect(actions[1]).toEqual({ type: 'POST_CAR' });
+      });
+  });
+});
 
-// describe('when user updates a car part', () => {
-//   it('fires update part action', async () => {
-//     const store = mockStore({});
-//     return store.dispatch(updatePart({}))
-//       .then(() => {
-//         const actions = store.getActions();
-//         expect(actions[0]).toEqual({ type: 'UPDATE_PART' });
-//       });
-//   });
-// });
+describe('when user deletes car', () => {
+  it('fires a delete car action', async () => {
+    const store = mockStore({});
+    axios.delete = jest.fn()
+    return store.dispatch(deleteCar(1, 4))
+      .then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({ payload: 3, type: 'SET_FILTER_INDEX' });
+        expect(actions[1]).toEqual({ payload: 1, type: 'DELETE_CAR' });
+      });
+  });
+});
 
-// describe('when user loads all cars', () => {
-//   global.fetch = jest.fn(() => Promise.resolve({
-//     json: () => Promise.resolve([]),
-//   }));
-
-//   it('fires get cars action', async () => {
-//     const store = mockStore([]);
-//     return store.dispatch(getCars())
-//       .then(() => {
-//         const actions = store.getActions();
-//         expect(actions[0]).toEqual({ type: 'GET_CARS' });
-//       });
-//     // .then((response) => console.log('response', response));
-//   });
-// });
+describe('when user updates a car part', () => {
+  it('fires update part action', async () => {
+    const resp = { part: {} };
+    axios.put = jest.fn()
+    axios.put.mockResolvedValue(resp);
+    const store = mockStore({});
+    return store.dispatch(updatePart(1, 2, {part: {}}))
+      .then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({ type: 'UPDATE_PART' });
+      });
+  });
+});
